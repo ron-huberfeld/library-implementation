2dfc2df50a39f52029cd2a838f420a03d4bc3cea
Gad Akuka <gad@segasec.com>
2018-08-07 13:35:50 +0300

    Generate entity "Author"

---
+++ src/main/java/com/interview/library/repository/AuthorRepository.java
@@ -0,0 +1,16 @@
+package com.interview.library.repository;
+
+import com.interview.library.domain.Author;
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.jpa.repository.*;
+
+
+/**
+ * Spring Data JPA repository for the Author entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+}
